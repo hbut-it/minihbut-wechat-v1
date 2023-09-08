8196584b0febe26ee3353678f4db2ae4ac448ea0
Souta <im@souta.cc>
2023-09-09 00:37:46 +0800

    feat: 新增自定义导航栏

---
+++ miniprogram/custom-tab-bar/index.ts
@@ -0,0 +1,24 @@
+// custom-tab-bar/index.ts
+Component({
+  data: {
+    value: 0,
+    list: [
+      { value: 0, pagePath: "pages/index/index", icon: "home", ariaLabel: "首页" },
+      { value: 1, pagePath: "pages/calendar/calendar", icon: "calendar", ariaLabel: "课表" },
+      { value: 2, pagePath: "pages/user/user", icon: "user", ariaLabel: "我的" },
+    ]
+  },
+
+  methods: {
+    init () {
+      const page = getCurrentPages().pop();
+      let a = this.data.list.find(item => item.pagePath === page.route)
+      this.setData({ value: a.value })
+    },
+
+    onChange (e: any) {
+      this.setData({ value: e.detail.value })
+      wx.switchTab({ url: '/' + this.data.list[e.detail.value].pagePath })
+    }
+  }
+})
